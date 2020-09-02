7d1a56a6b6607fbf75dc860b753ebce00e825064
muriloviscondi <muriloviscondi@gmail.com>
2020-09-02 11:05:32 -0300

    configurango BookServices

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -15,5 +15,27 @@ namespace BooksApi.Services
 
       _books = database.GetCollection<Book>(settings.BooksCollectionName)
     }
+
+    public List<Book> Get() =>
+      _books.Find(book => true).ToList();
+
+    public Book Get(string id) =>
+      _books.Find<Book>(book => book.Id == id).FirstOfDefault();
+
+    public Book Create(Book book)
+    {
+      _books.InsertOne(book);
+      return book;
+    }
+
+    public void Update(string id, Book bookIn) =>
+      _books.replaceOne(book => book.Id == id, bookIn)
+
+    public void Remove(Book bookIn) =>
+      _books.DeleteOne(book => book.Id == bookIn.Id)
+
+    public void Remove(string id) =>
+      _books.DeleteOne(book => book.Id == id)
+
   }
 }
